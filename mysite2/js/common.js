69d83f305e39ee94c74c2ce1b12eee71ebdfcda3
TabulaRasa13 <dbnzz@mail.ru>
2017-09-17 00:10:38 +0300

    click anim

--- mysite2/js/common.js
+++ mysite2/js/common.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 	
+	
+	$(".wrapper").on('click',function(){
+		$(".anima").addClass("anim-a");
+		$(".anim4").addClass("anim-a4");
+		$(".anim2").addClass("anim-a2");
+		$(".anim3").addClass("anim-a3");
+		$(".anim5").addClass("anim-a5");
+		$(".animb").addClass("anim-b");
+		$(".anim6").addClass("anim-b2");
+		$(".anim7").addClass("anim-b3");
+		$(".anim8").addClass("anim-b4");
+		$(".wrap").addClass("wrap-anim");
+		$(".stara").addClass("star1");
+		$(".starb").addClass("star2");
+		$(".stars").addClass("starsz");
+		$(".wrap").addClass("wrap-opac");
+});
+	
 
 	
 	

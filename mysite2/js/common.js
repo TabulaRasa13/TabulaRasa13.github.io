ee2fce25e83a0cc61b5de8ba2eab1904306f6929
TabulaRasa13 <dbnzz@mail.ru>
2017-09-17 00:35:00 +0300

    click anim v2

--- mysite2/js/common.js
+++ mysite2/js/common.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
 	
 	
 	$(".wrapper").on('click',function(){
-		$(".anima").addClass("anim-a");
-		$(".anim4").addClass("anim-a4");
-		$(".anim2").addClass("anim-a2");
-		$(".anim3").addClass("anim-a3");
-		$(".anim5").addClass("anim-a5");
-		$(".animb").addClass("anim-b");
-		$(".anim6").addClass("anim-b2");
-		$(".anim7").addClass("anim-b3");
-		$(".anim8").addClass("anim-b4");
-		$(".wrap").addClass("wrap-anim");
-		$(".stara").addClass("star1");
-		$(".starb").addClass("star2");
-		$(".stars").addClass("starsz");
-		$(".wrap").addClass("wrap-opac");
+		$(".anima").toggleClass("anim-a");
+		$(".anim4").toggleClass("anim-a4");
+		$(".anim2").toggleClass("anim-a2");
+		$(".anim3").toggleClass("anim-a3");
+		$(".anim5").toggleClass("anim-a5");
+		$(".animb").toggleClass("anim-b");
+		$(".anim6").toggleClass("anim-b2");
+		$(".anim7").toggleClass("anim-b3");
+		$(".anim8").toggleClass("anim-b4");
+		$(".wrap").toggleClass("wrap-anim");
+		$(".stara").toggleClass("star1");
+		$(".starb").toggleClass("star2");
+		$(".stars").toggleClass("starsz");
+		$(".wrap").toggleClass("wrap-opac");
 });
 	
 
